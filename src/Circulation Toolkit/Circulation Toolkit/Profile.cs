0a30e11525bb544c82ceba9a028da4a6b9cbde28
jaggyluis <luis.jaggy@gmail.com>
2016-08-30 17:57:24 -0700

    begin building c#

---
+++ src/Circulation Toolkit/Circulation Toolkit/Profile.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Circulation_Toolkit
+{
+    /// <summary>
+    /// Main Profile class for storing input information
+    /// from the Rhino environment
+    /// </summary>
+    class Profile
+    {
+        public string Name;
+        public string Type;
+        public Dictionary<string, string> Attributes;
+
+        public Profile(string name, string type)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Attributes = new Dictionary<string, string>();
+        }
+
+        public bool HasAttribute (string attribute)
+        {
+            if (this.Attributes.ContainsKey(attribute))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasValue (string value)
+        {
+            if (this.Attributes.ContainsValue(value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// FloorProfile sub-class that stores Floor information
+    /// fron the Rhino environment
+    /// </summary>
+    class FloorProfile : Profile
+    {
+        private Curve _boundary;
+
+        public FloorProfile(string name, Curve boundary)
+            : base (name, "floor")
+        {
+            this._boundary = boundary;
+        }
+
+        public Curve Boundary
+        {
+            get
+            {
+                return this._boundary;
+            }
+        }
+    }
+}
---
+++ src/Circulation Toolkit/Circulation Toolkit/ToolkitInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Circulation_Toolkit
+{
+    public class ToolkitInfo : GH_AssemblyInfo
+  {
+    public override string Name
+    {
+        get
+        {
+            return "Circulation";
+        }
+    }
+    public override Bitmap Icon
+    {
+        get
+        {
+            //Return a 24x24 pixel bitmap to represent this GHA library.
+            return null;
+        }
+    }
+    public override string Description
+    {
+        get
+        {
+            //Return a short string describing the purpose of this GHA library.
+            return "";
+        }
+    }
+    public override Guid Id
+    {
+        get
+        {
+            return new Guid("60e51639-59fe-4841-925b-1b80594493b3");
+        }
+    }
+
+    public override string AuthorName
+    {
+        get
+        {
+            //Return a string identifying you or your company.
+            return "Microsoft";
+        }
+    }
+    public override string AuthorContact
+    {
+        get
+        {
+            //Return a string representing your preferred contact details.
+            return "";
+        }
+    }
+}
+}
